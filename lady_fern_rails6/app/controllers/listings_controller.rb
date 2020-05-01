3b6e3041781d79131dc4bd7c915d4f8990133500
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-05-01 22:03:35 +1000

    flushed out listings_controller methods

--- lady_fern_rails6/app/controllers/listings_controller.rb
+++ lady_fern_rails6/app/controllers/listings_controller.rb
@@ -2,21 +2,48 @@ class ListingsController < ApplicationController
     before_action :authenticate_user!
 
     def index
+        @listings = Listing.all
     end
 
     def show
     end
 
     def new
+        @listing = Listing.new
     end
 
     def create
+        @listing = current_user.listings.create(listing_params)
+        if @listing.errors.any?
+            # render "new"
+        else 
+            redirect_to listings_path
+        end
     end
 
     def update
     end
 
     def destroy
+        Listing.find(params[:id]).destroy
+        redirect_to listings_path
     end
-    
+
+    private
+
+    def set_listing
+        @listing = Listing.find(params[:id])
+    end
+
+    def set_user_listing
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        if @listing == nil
+            redirect_to listings_path
+        end
+    end
+
+    def listing_params
+        params.require(:listing).permit(:title, :description, :user_id, :size, :price, :in_stock, :picture)
+    end
 end
