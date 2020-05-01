eb2a5d4575c60b8a0ee6de3c3c6da9456e4c1ab1
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-05-01 21:54:39 +1000

    added devise

---
+++ lady_fern_rails6/app/controllers/listings_controller.rb
@@ -0,0 +1,22 @@
+class ListingsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
+end
