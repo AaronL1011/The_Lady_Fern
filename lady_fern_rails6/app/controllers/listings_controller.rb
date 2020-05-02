a7e27eb330c62ee735e368e28f01065cb614b627
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-05-02 21:16:40 +1000

    began index view formatting

--- lady_fern_rails6/app/controllers/listings_controller.rb
+++ lady_fern_rails6/app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:new, :destroy, :update]
 
     def index
         @listings = Listing.all
