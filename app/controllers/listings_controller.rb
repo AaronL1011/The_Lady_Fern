52512a4f41635877d0615da8f5769cfc7ca1d3e7
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-05-16 10:04:19 +1000

    more cache

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -86,12 +86,5 @@ class ListingsController < ApplicationController
         params.require(:listing).permit(:title, :description, :user_id, :size, :price, :in_stock, :picture)
     end
 
-    def get_faux_listings
-        faux_listings = []
-        object = { id: 8, listing_id: 17, user_id: 1, title: nil, description: nil, price: nil, in_stock: nil, created_at: "2020-05-15 10:18:14", updated_at: "2020-05-15 10:18:14", size: nil}
-        for i in 0..2
-            p faux_listings.push(object)
-        end
-        return faux_listings
-    end
+    
 end
